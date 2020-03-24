554708326869194952a894b2478e2146de5630f7
agarwalastha <asthaagarwal1001@gmail.com>
2020-03-25 03:45:58 +0530

    Links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { WorldWideComponent } from './world-wide/world-wide.component';
+import {HelplineComponent} from './helpline/helpline.component';
+import { TestCentresComponent } from './test-centres/test-centres.component';
 
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent},
-  { path: 'world-wide', component: WorldWideComponent}
+  { path: 'helpline', component: HelplineComponent},
+  { path: 'testcenter', component: TestCentresComponent}
 ];
 
 @NgModule({
