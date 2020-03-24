bdc357a5dfc541f5410b2251db82d922ee5c7599
agarwalastha <asthaagarwal1001@gmail.com>
2020-03-24 16:48:46 +0530

    Age-Gender and Data Labels

--- src/app/age-gender-india/age-gender-india.component.ts
+++ src/app/age-gender-india/age-gender-india.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { ChartDataSets, ChartOptions, ChartType } from 'chart.js';
+import * as pluginDataLabels from 'chartjs-plugin-datalabels';
 import { Label } from 'ng2-charts';
+
+import {HomeService} from '../home/home.service';
+import { analyzeAndValidateNgModules } from '@angular/compiler';
+import { PassThrough } from 'stream';
 
 @Component({
   selector: 'app-age-gender-india',
@@ -8,23 +13,115 @@ import { Label } from 'ng2-charts';
   styleUrls: ['./age-gender-india.component.css']
 })
 export class AgeGenderIndiaComponent implements OnInit {
+  genderData: any;
+  ageGroup: any;
+  maleAgeData: Array<any> = [];
+  femaleAgeData: Array<any> = [];
+
+  public maleAgeGroup = {
+    '1-10': 0,
+    '11-20': 0,
+    '21-30': 0,
+    '31-40': 0,
+    '41-50': 0,
+    '51-60': 0,
+    '61-70': 0,
+    '71-80': 0,
+    '81-100': 0
+  };
+
+  public femaleAgeGroup = {
+    '1-10': 0,
+    '11-20': 0,
+    '21-30': 0,
+    '31-40': 0,
+    '41-50': 0,
+    '51-60': 0,
+    '61-70': 0,
+    '71-80': 0,
+    '81-100': 0
+  };
 
   public barChartOptions: ChartOptions = {
     responsive: true,
   };
-  public barChartLabels: Label[] = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+  public barChartLabels: Label[] = ['1-10', '11-20', '21-30', '31-40', '41-50', '51-60', '61-70', '71-80', '81-100'];
   public barChartType: ChartType = 'bar';
   public barChartLegend = true;
-  public barChartPlugins = [];
+  public barChartPlugins = [pluginDataLabels];
 
   public barChartData: ChartDataSets[] = [
-    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
-    { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' }
+    { data: [], label: 'Male' },
+    { data: [], label: 'Female' }
   ];
 
-  constructor() { }
+  constructor(private homeService: HomeService) { }
 
   ngOnInit() {
+    this.homeService.GetPatientData()
+    .subscribe(data => {
+      this.genderData = data.raw_data;
+      this.getGenderStats(this.genderData);
+    });
   }
 
-}
+  public getGenderStats(genderData: any) {
+    genderData.map((obj) => {
+      if (obj.gender) {
+        if (obj.agebracket) {
+              this.ageGroup = this.getAgeGroup(obj.agebracket);
+              if (obj.gender === 'M') {
+                this.maleAgeGroup[this.ageGroup] += 1;
+              } else {
+                this.femaleAgeGroup[this.ageGroup] += 1;
+              }
+        }
+      }
+  });
+
+    this.maleAgeData =  Object.keys(this.maleAgeGroup).map((key) => {
+        return this.maleAgeGroup[key];
+    });
+
+    this.femaleAgeData = Object.keys(this.femaleAgeGroup).map((key) => {
+      return this.femaleAgeGroup[key];
+  });
+
+    this.barChartData[0].data = this.maleAgeData;
+    this.barChartData[1].data = this.femaleAgeData;
+
+  }
+
+  public getAgeGroup(ageData: any) {
+    let age;
+    age = Number(ageData);
+    let ageGroup;
+    if (age < 51) {
+          if (age < 11) {
+              ageGroup = '1-10';
+          } else if (age < 21) {
+              ageGroup = '11-20';
+          } else if (age < 31) {
+              ageGroup = '21-30';
+          } else if (age < 41) {
+              ageGroup = '31-40';
+          } else {
+              ageGroup = '41-50';
+          }
+      } else {
+          if (age < 61) {
+              ageGroup = '51-60';
+          } else if (age < 71) {
+              ageGroup = '61-70';
+          } else if (age < 81) {
+              ageGroup = '71-80';
+          } else {
+              ageGroup = '81-100';
+          }
+      }
+    return ageGroup;
+  }
+
+  }
+
+
