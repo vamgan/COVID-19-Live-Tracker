eb72d242e35211f3cd0e761cee39d51dd171a3ec
vamgan <13998000+vamgan@users.noreply.github.com>
2020-03-26 20:26:07 +0530

    Chart

--- src/app/worldwidebar/worldwidebar.component.ts
+++ src/app/worldwidebar/worldwidebar.component.ts
@@ -9,33 +9,55 @@ import { WorldwideService } from '../worldwide/worldwide.service';
   styleUrls: ['./worldwidebar.component.css']
 })
 export class WorldwidebarComponent implements OnInit {
-  WorldData: any;
+  WorldDataC: any;
   public barChartOptions: ChartOptions = {
     responsive: true,
+    // We use these empty structures as placeholders for dynamic theming.
+    scales: { xAxes: [{}], yAxes: [{}] },
+    plugins: {
+      datalabels: {
+        anchor: 'end',
+        align: 'end',
+      }
+    }
   };
   public barChartLabels: Label[] = [];
   public barChartType: ChartType = 'bar';
   public barChartLegend = true;
   public barChartPlugins = [];
 
-  public barChartData: ChartDataSets[] = [];
-  countstate: number;
-
+  public barChartData: ChartDataSets[] = [
+    { data: [], label: 'Confirmed Cases' },
+    { data: [], label: 'Recovered Cases' }
+  ];
+  WorldDataR: any;
   constructor(private worldservice: WorldwideService) { }
 
   ngOnInit() {
-    this.worldservice.GetConfirmedData().subscribe((WorldData) => {
-    this.WorldData = WorldData;
-    this.getConfirmedCases(this.WorldData);
+    this.worldservice.GetConfirmedData().subscribe((WorldDataC) => {
+    this.WorldDataC = WorldDataC;
+    this.getConfirmedCases(this.WorldDataC);
     });
+    this.worldservice.GetRecoveredData().subscribe((WorldDataR) => {
+      this.WorldDataR = WorldDataR;
+      this.getRecoveredCases(this.WorldDataR);
+      });
+
   }
 
 
-  public getConfirmedCases(WorldData: any) {
+  public getConfirmedCases(WorldDataC: any) {
     let i = 0;
     for (i = 0 ; i < 10; i++) {
-        this.barChartLabels.push(WorldData[i].country);
-        this.barChartData.push(WorldData[i].cases);
+        this.barChartLabels.push(WorldDataC[i].country);
+        this.barChartData[0].data.push(WorldDataC[i].cases);
+    }
+
+  }
+  public getRecoveredCases(WorldDataR: any) {
+    let i = 0;
+    for (i = 0 ; i < 10; i++) {
+        this.barChartData[1].data.push(WorldDataR[i].recovered);
     }
 
   }
