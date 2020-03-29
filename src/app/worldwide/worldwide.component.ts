2727583fcb60c068aa598f6f5ee1e4450c46fea8
agarwalastha <asthaagarwal1001@gmail.com>
2020-03-29 21:59:07 +0530

    china fix

--- src/app/worldwide/worldwide.component.ts
+++ src/app/worldwide/worldwide.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import {NgbPagination } from '@ng-bootstrap/ng-bootstrap';
 import {HttpClient} from '@angular/common/http';
+import {WorldwideService} from './worldwide.service';
 
 @Component({
   selector: 'app-worldwide',
@@ -11,16 +12,19 @@ import {HttpClient} from '@angular/common/http';
 export class WorldwideComponent implements OnInit {
   WorldData: any;
   CountryData: any;
+  CountryDataC: any;
   page = 1;
   pageSize = 15;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private worldService: WorldwideService) { }
 
   ngOnInit() {
       this.http.get('https://corona.lmao.ninja/all')
       .subscribe((WorldData) => this.WorldData = WorldData );
       this.http.get('https://corona.lmao.ninja/countries')
       .subscribe((CountryData) => this.CountryData = CountryData );
+      this.worldService.GetConfirmedData()
+      .subscribe(data => this.CountryDataC = data );
 
   }
 
