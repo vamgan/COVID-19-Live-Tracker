c5035138a01a72447cd7cdc847df1cbc5b5f5b62
agarwalastha <asthaagarwal1001@gmail.com>
2020-03-30 21:29:31 +0530

    movies table

--- src/app/funzone/movies/movies.component.ts
+++ src/app/funzone/movies/movies.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-movies',
@@ -6,10 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movies.component.css']
 })
 export class MoviesComponent implements OnInit {
+  moviesData: any;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.http.get('https://api.gocovid19.in/movies.json')
+    .subscribe(data => this.moviesData = data);
   }
 
 }
