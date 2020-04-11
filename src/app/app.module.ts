9e0a486aa001c21b47bf7ee41275f3d4baaf5246
vamgan <13998000+vamgan@users.noreply.github.com>
2020-04-11 19:11:56 +0530

    map

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, NO_ERRORS_SCHEMA  } from '@angular/core';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {HttpClientModule} from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -16,12 +16,12 @@ import { GenderIndiaChartComponent } from './home/gender-india-chart/gender-indi
 import { AgeGenderIndiaComponent } from './home/age-gender-india/age-gender-india.component';
 import { SourcesComponent } from './sources/sources.component';
 import { FooterComponent } from './footer/footer.component';
-import {WorldwideComponent} from './worldwide/worldwide.component';
+import { WorldwideComponent } from './worldwide/worldwide.component';
 import { WorldwidebarComponent } from './worldwidebar/worldwidebar.component';
 import { DailyindComponent } from './home/dailyind/dailyind.component';
 import { NewsTickerComponent } from './home/news-ticker/news-ticker.component';
-import {NgbPaginationModule, NgbToastModule} from '@ng-bootstrap/ng-bootstrap';
-import {NgbTabsetModule} from '@ng-bootstrap/ng-bootstrap';
+import { NgbPaginationModule, NgbToastModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbTabsetModule } from '@ng-bootstrap/ng-bootstrap';
 import { SlickCarouselModule } from 'ngx-slick-carousel';
 import { NewsComponent } from './news/news.component';
 import { FunzoneComponent } from './funzone/funzone.component';
@@ -30,7 +30,10 @@ import { MoviesComponent } from './funzone/movies/movies.component';
 import { TestedchartComponent } from './home/testedchart/testedchart.component';
 import { HighchartsChartModule } from 'highcharts-angular';
 import { MapComponent } from './home/map/map.component';
-
+import { ChartModule, HIGHCHARTS_MODULES } from 'angular-highcharts';
+import * as more from 'highcharts/highcharts-more.src';
+import * as exporting from 'highcharts/modules/exporting.src';
+import * as highmaps from 'highcharts/modules/map.src';
 
 @NgModule({
   declarations: [
@@ -71,7 +74,9 @@ import { MapComponent } from './home/map/map.component';
     NgbToastModule
 
   ],
-  providers: [],
+  providers: [{ provide: HIGHCHARTS_MODULES, useFactory: () => [more, exporting] },
+  { provide: HIGHCHARTS_MODULES, useFactory: () => [highmaps] }
+],
   bootstrap: [AppComponent],
   schemas: [
     NO_ERRORS_SCHEMA
--- src/app/home/map/map.component.ts
+++ src/app/home/map/map.component.ts
@@ -1,15 +1,55 @@
+declare var require: any;
+
 import { Component, OnInit } from '@angular/core';
+import * as Highcharts from 'highcharts';
+import MapModule from 'highcharts/modules/map';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+const Uganda = require('@highcharts/map-collection/countries/ug/ug-all.geo.json');
+MapModule(Highcharts);
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.css']
 })
-export class MapComponent implements OnInit {
+export class MapComponent  {
+  title = 'app';
+  chart;
+  updateFromInput = false;
+  Highcharts = Highcharts;
+  chartConstructor = 'mapChart';
+  chartCallback;
+  chartOptions = {
+    chart: {
+      map: Uganda
+    },
+    title: {
+      text: 'Highmaps basic demo'
+    },
+    mapNavigation: {
+      enabled: true,
+      buttonOptions: {
+        alignTo: 'spacingBox'
+      }
+    },
+    colorAxis: {
+      min: 0
+    },
+    series: [{
+      name: 'Random data',
+      states: {
+        hover: {
+          color: '#BADA55'
+        }
+      },
+      dataLabels: {
+        enabled: true,
+        format: '{point.name}'
+      },
+      allAreas: false,
+      data: [
+      ]
+   }]};
 
-  constructor() { }
-
-  ngOnInit(): void {
   }
-
-}
